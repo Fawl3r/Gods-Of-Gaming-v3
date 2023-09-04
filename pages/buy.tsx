db180fb421af80e615695d667da618a634e9c6bf
Fawl3r <Fawl3r@gmail.com>
2023-09-03 21:17:03 -0500

    up

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -1,28 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useContract, useNFTs } from "@thirdweb-dev/react";
 import Container from "../components/Container/Container";
 import NFTGrid from "../components/NFT/NFTGrid";
 import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
 
+// Placeholder function, replace with your actual logic
+const ValidDirectListings = (nft) => {
+  // Replace this with your own logic to determine if a listing is valid
+  return true; // for now, we assume all NFTs are valid listings
+};
+
 export default function Buy() {
   // Load all of the NFTs from the NFT Collection
   const { contract } = useContract(NFT_COLLECTION_ADDRESS);
   const { data, isLoading, error } = useNFTs(contract, {
-    count: 100,
+    count: 1000,
     start: 0,
   });
 
   // Pagination state variables
   const [currentPage, setCurrentPage] = useState(1);
-  const [nftsPerPage] = useState(50); // You can set the number of NFTs per page here
+  const [nftsPerPage] = useState(50);
+  const [validNfts, setValidNfts] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      // Filter out invalid NFTs
+      const filteredNfts = data.filter(ValidDirectListings);
+      setValidNfts(filteredNfts);
+    }
+  }, [data]);
 
   // Calculate the total number of pages
-  const totalPages = data ? Math.ceil(data.length / nftsPerPage) : 0;
+  const totalPages = validNfts ? Math.ceil(validNfts.length / nftsPerPage) : 0;
 
   // Get the NFTs for the current page
   const indexOfLastNft = currentPage * nftsPerPage;
   const indexOfFirstNft = indexOfLastNft - nftsPerPage;
-  const currentNfts = data ? data.slice(indexOfFirstNft, indexOfLastNft) : [];
+  const currentNfts = validNfts.slice(indexOfFirstNft, indexOfLastNft);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -31,8 +46,7 @@ export default function Buy() {
       <h1>Buy Warrior NFTs</h1>
       <p>Browse which NFTs are available from the collection.</p>
       {error ? (
-       <p>Error: {(error as Error).message}</p>
-
+        <p>Error: {(error as Error).message}</p>
       ) : (
         <NFTGrid data={currentNfts} isLoading={isLoading} emptyText={"Looks like there are no NFTs in this collection."} />
       )}
