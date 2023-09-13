fcca228ecc3a4fdec0ecf0da3a91b0dd74f27e18
Fawl3r <Fawl3r@gmail.com>
2023-09-13 17:08:14 -0500

    up

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -7,10 +7,10 @@ export const NETWORK = Binance;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x2bBcbc65db2A5c1Fd11CFd173786Aa9Fa3fE792f";
+export const MARKETPLACE_ADDRESS = "0x2bBcbc65db2A5c1Fd11CFd173786Aa9Fa3fE792f"; 
 
 // 3. The address of your NFT collection smart contract.
-export const NFT_COLLECTION_ADDRESS = "0xB8a33200A4a09d8c5F5313D6a19A9609B916479e";
+export const NFT_COLLECTION_ADDRESS = "0xB8a33200A4a09d8c5F5313D6a19A9609B916479e"; 
 
 // (Optional) Set up the URL of where users can view transactions on
 // For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
