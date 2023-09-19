da3bcfdc95a14ed76973956d0902e689334a35ef
Fawl3r <Fawl3r@gmail.com>
2023-09-19 09:47:46 -0500

    up

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -1,37 +1,36 @@
-import {
-  ConnectWallet,
-} from "@thirdweb-dev/react";
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { useContract, useValidDirectListings } from '@thirdweb-dev/react';
+import { useContract, useNFTs } from '@thirdweb-dev/react';
 import Container from '../components/Container/Container';
 import NFTGrid from '../components/NFT/NFTGrid';
 import { NFT_COLLECTION_ADDRESS } from '../const/contractAddresses';
-import styles from "../styles/Home.module.css";
+
+
+
+const ValidDirectListings = (nft) => {
+  // Replace this with your own logic to determine if a listing is valid
+  return true;
+};
 
 export default function Buy() {
   const router = useRouter();
   const currentPage = parseInt(router.query.page as string) || 1;
-  const [sortOrder, setSortOrder] = useState('asc'); // 'asc' for low to high, 'desc' for high to low
 
-  const { contract } = useContract(NFT_COLLECTION_ADDRESS, "marketplace-v3");
-  const { data, isLoading, error } = useValidDirectListings(contract);
+  const { contract } = useContract(NFT_COLLECTION_ADDRESS);
+  const { data, isLoading, error } = useNFTs(contract, {
+    count: 1000,
+    start: 0,
+  });
 
   const [nftsPerPage] = useState(50);
   const [validNfts, setValidNfts] = useState([]);
 
   useEffect(() => {
     if (data) {
-      const sortedData = [...data].sort((a, b) => {
-        if (sortOrder === 'asc') {
-          return parseFloat(a.pricePerToken) - parseFloat(b.pricePerToken);
-        } else {
-          return parseFloat(b.pricePerToken) - parseFloat(a.pricePerToken);
-        }
-      });
-      setValidNfts(sortedData);
+      const filteredNfts = data.filter(ValidDirectListings);
+      setValidNfts(filteredNfts);
     }
-  }, [data, sortOrder]);
+  }, [data]);
 
   const totalPages = validNfts ? Math.ceil(validNfts.length / nftsPerPage) : 0;
   const indexOfLastNft = currentPage * nftsPerPage;
@@ -41,7 +40,7 @@ export default function Buy() {
   const setCurrentPage = (page: number) => {
     router.push({
       pathname: '/buy',
-      query: { page },
+      query: { page }
     });
   };
 
@@ -50,29 +49,26 @@ export default function Buy() {
   };
 
   return (
-    <div className={styles.container}>
-        
     <Container maxWidth="lg">
-    <div className={styles.connectWalletButton}>
-          <ConnectWallet />
-        </div>
       <h1>Buy Warrior NFTs</h1>
-
-      {/* Sort Controls */}
-      <div>
-        <button onClick={() => setSortOrder('asc')}>Price Low to High</button>
-        <button onClick={() => setSortOrder('desc')}>Price High to Low</button>
-      </div>
 
       {/* Pagination Controls */}
       <div>
-        <button onClick={() => paginate(1)} disabled={currentPage === 1}>First</button>
-        <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>Prev</button>
+        <button onClick={() => paginate(1)} disabled={currentPage === 1}>
+          First
+        </button>
+        <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+          Prev
+        </button>
         <span>Page {currentPage} of {totalPages}</span>
-        <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
-        <button onClick={() => paginate(totalPages)} disabled={currentPage === totalPages}>Last</button>
+        <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>
+          Next
+        </button>
+        <button onClick={() => paginate(totalPages)} disabled={currentPage === totalPages}>
+          Last
+        </button>
       </div>
-
+      
       <p>Browse which NFTs are available from the collection.</p>
       {error ? (
         <p>Error: {(error as Error).message}</p>
@@ -80,6 +76,5 @@ export default function Buy() {
         <NFTGrid data={currentNfts} isLoading={isLoading} emptyText={"Looks like there are no NFTs in this collection."} />
       )}
     </Container>
-    </div> 
   );
 }
